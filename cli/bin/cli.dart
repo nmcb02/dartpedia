942665b5e57268033ac1cf1a03ac8a4ed60d97ca
Naomi Born <naomimcborn@gmail.com>
2026-07-13 11:42:34 +0200

    first commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,15 @@
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Hello world!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
+}
+
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
